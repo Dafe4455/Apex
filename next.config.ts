95f8034144e7fe8577b41c08e6da24eb5efc2c40
hvriza39-rgb <hvriza39@gmail.com>
2026-06-09 20:56:16 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-// before
-import createNextIntlPlugin from "next-intl/plugin";
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
-export default withNextIntl({ /* your config */ });
+import type { NextConfig } from "next";
 
-// after — just your plain config
-export default { /* your config */ };
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
